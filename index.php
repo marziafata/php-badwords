115ae88cdc8660713b8bc1cac9266ff664b0be4e
Marzia Fatarella <marziafatarella@gmail.com>
2020-06-16 09:48:56 +0200

    Aggiunta lunghezza

--- index.php
+++ index.php
@@ -34,20 +34,17 @@
                 ?>
 
             </p>
-            <!-- <p>
-
-                //<?php
-                    // $testo2 = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-                    //
-                    // $testo_esploso = explode(' ', $testo2);
-                    //
-                    // for ($i=0; $i < $testo_esploso ; $i++) {
-                    //     $parola_corrente = $testo_esploso[$i];
-                    //     echo $parola corrente;
-                    // }
-
-                // ?>
-            </p> -->
+            <p>
+                <strong>
+                    Questo paragrafo è lungo:
+                    <?php
+                    //stampo la lunghezza del testo
+                    $lunghezza = strlen($testo);
+                    echo $lunghezza;
+                    ?>
+                    caratteri.
+                </strong>
+            </p>
         </div>
     </body>
 </html>
